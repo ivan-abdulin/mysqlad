825d115cd93093ccfcd2f2722840a2afc210965b
Ivan Abdulin <ivan.abdulin@gmail.com>
2016-07-05 19:50:05 +0300

    Added basic sass structure. Added gulp tasks to compile sass.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,28 @@
 'use strict';
 
 import gulp from 'gulp';
+import sass from 'gulp-sass';
+
+/**
+ * Compile and copy styles
+ */
+gulp.task('styles', () => {
+    return gulp.src('app/Resources/sass/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('web/css'));
+});
+
+
+/**
+ * Watch tasks
+ *
+ * Remember "watch" only spots added/removed files when source paths are relative
+ */
+gulp.task('watch', () => {
+    gulp.watch('app/Resources/sass/**/*.scss', ['styles']);
+});
 
 gulp.task('default', [
+    'styles',
+    'watch'
 ]);
