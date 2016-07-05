997ee127a8fa9a74a3b8627a8abe08fbc5a02475
Ivan Abdulin <ivan.abdulin@gmail.com>
2016-07-06 00:41:27 +0300

    Added sourcemaps for css. Added environment support.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,25 @@
 
 import gulp from 'gulp';
 import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import gulpif from 'gulp-if';
+
+var environment = null;
+
+/**
+ * Set environment
+ */
+gulp.task('setEnvDev',  () => { environment = 'dev'; });
+gulp.task('setEnvProd', () => { environment = 'prod'; });
 
 /**
  * Compile and copy styles
  */
 gulp.task('styles', () => {
     return gulp.src('app/Resources/sass/app.scss')
+        .pipe(gulpif(environment === 'dev', sourcemaps.init()))
         .pipe(sass())
+        .pipe(gulpif(environment === 'dev', sourcemaps.write()))
         .pipe(gulp.dest('web/css'));
 });
 
@@ -23,6 +35,12 @@ gulp.task('watch', () => {
 });
 
 gulp.task('default', [
+    'setEnvDev', // synchronous
     'styles',
     'watch'
+]);
+
+gulp.task('deploy', [
+    'setEnvProd',
+    'styles'
 ]);
